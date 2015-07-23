09801389071926bd45086d9baf69c2cdc523671b
zpbappi <zpbappi@gmail.com>
2015-07-23 21:49:40 +1000

    added first test and trivial implementation to pass it

--- TddCalculator/Stack.Tests/StackTest.fs
+++ TddCalculator/Stack.Tests/StackTest.fs
@@ -1,5 +1,17 @@
 ﻿module Stack.Tests
 
+open Xunit
+open Swensen.Unquote
 open Stack
 
+[<Fact>]
+let ``Stack should be empty for empty list`` () =
+    test <@ isEmpty [] @>
 
+[<Fact>]
+let ``Stack should not be empty for non-empty list`` () =
+    ()
+
+[<Fact>]
+let ``empty checking should work for stack of any arbitrary type`` () =
+    ()
--- TddCalculator/Stack/Stack.fs
+++ TddCalculator/Stack/Stack.fs
@@ -2,3 +2,5 @@
 
 type Stack<'a> = 'a list
 
+let isEmpty (stack : Stack<'a>) =
+    true
