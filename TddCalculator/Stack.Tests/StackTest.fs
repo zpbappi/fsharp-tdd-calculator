7c06918c6297090c6e6a008a23704ebd8e63e3e9
zpbappi <zpbappi@gmail.com>
2015-07-24 09:24:15 +1000

    completed generic empty checking test just to see it pass

--- TddCalculator/Stack.Tests/StackTest.fs
+++ TddCalculator/Stack.Tests/StackTest.fs
@@ -12,6 +12,23 @@ let ``Stack should be empty for empty list`` () =
 let ``Stack should not be empty for non-empty list`` () =
     test <@ isEmpty [1; 2; 3] = false @>
 
+
+type Student = {
+    Name : string
+    Roll : int
+    CGPA : float
+}
 [<Fact>]
 let ``empty checking should work for stack of any arbitrary type`` () =
-    ()
+    let intStack = [1; 2; 3]
+    let strStack = ["a"; "b"]
+    let floatStack = [1.; 3.9]
+    let studentStack = [
+        {Name = "Mr. A"; Roll = 1; CGPA = 4.0};
+        {Name = "Mr. B"; Roll = 2; CGPA = 3.95}
+    ]
+
+    test <@ isEmpty intStack = false @>
+    test <@ isEmpty strStack = false @>
+    test <@ isEmpty floatStack = false @>
+    test <@ isEmpty studentStack = false @>
