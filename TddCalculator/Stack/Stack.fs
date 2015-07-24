ee8e4c975485b924dc161f565fb3a0a85d3f4a2a
zpbappi <zpbappi@gmail.com>
2015-07-24 16:52:58 +1000

    trivial implementation of push and pop methods

--- TddCalculator/Stack/Stack.fs
+++ TddCalculator/Stack/Stack.fs
@@ -6,3 +6,9 @@ let isEmpty (stack : Stack<'a>) =
     match stack with 
     | [] -> true
     | _ -> false
+
+let push (item : 'a) (stack : Stack<'a>) =
+    []
+
+let pop (stack : Stack<'a>) =
+    (0, [])
