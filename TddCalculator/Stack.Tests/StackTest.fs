7755cd148a25165b5bdbf6eb12b61d592ac73d2d
zpbappi <zpbappi@gmail.com>
2015-07-24 17:09:31 +1000

    failing tests for push and pop

--- TddCalculator/Stack.Tests/StackTest.fs
+++ TddCalculator/Stack.Tests/StackTest.fs
@@ -32,3 +32,20 @@ let ``empty checking should work for stack of any arbitrary type`` () =
     test <@ isEmpty strStack = false @>
     test <@ isEmpty floatStack = false @>
     test <@ isEmpty studentStack = false @>
+
+
+[<Fact>]
+let ``pushing to empty stack return stack with single element`` () =
+    test <@ push 1 [] = [1] @>
+
+[<Fact>]
+let ``pushing to any non-empty stack places the item at first`` () =
+    test <@ push 1 [2; 3] = [1; 2; 3] @>
+
+[<Fact>]
+let ``popping from single element stack returns the item and empty stack`` () =
+    test <@ pop [1] = (1, []) @>
+
+[<Fact>]
+let ``popping from arbitrary stack return the first item and rest of the stack`` () =
+    test <@ pop [1; 2; 3] = (1, [2; 3]) @>
