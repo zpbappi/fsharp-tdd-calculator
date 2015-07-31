7c0e1b9451ba1d5a2509352e555750bd75ad9aca
zpbappi <zpbappi@gmail.com>
2015-07-31 22:41:51 +1000

    tests for adding two numbers and passing tests

---
+++ TddCalculator/RpnCalculator.Tests/RpnCalculatorTests.fs
@@ -0,0 +1,22 @@
+﻿module RpnCalculatorTests
+
+open Xunit
+open Swensen.Unquote
+
+open RpnCalculator
+
+[<Fact>]
+let ``calculation on empty stack results in zero`` () =
+    test <@ calculate [] = 0 @>
+
+[<Fact>]
+let ``should be able to add two numbers from stack`` () =
+    test <@ calculate ["1"; "1"; "+"] = 2 @>
+
+[<Theory>]
+[<InlineData("1", "1", 2)>]
+[<InlineData("0", "0", 0)>]
+[<InlineData("1", "2", 3)>]
+[<InlineData("40", "2", 42)>]
+let ``should be able to add any arbitrary two integers`` (x : string) (y : string) (res: int) =
+    test <@ calculate [x; y; "+"] = res @>
---
+++ TddCalculator/RpnCalculator/RpnCalculator.fs
@@ -0,0 +1,9 @@
+﻿module RpnCalculator
+
+open Stack
+
+let calculate (stack : Stack<string>) =
+    match stack with
+    | [a; b; "+"] -> System.Int32.Parse(a) + System.Int32.Parse(b)
+    | _ -> 0
+
