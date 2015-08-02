375285ea4dc2e074108a3b30a787864fa202bedc
zpbappi <zpbappi@gmail.com>
2015-08-02 20:57:04 +1000

    created types for RpnCalculator

---
+++ TddCalculator/RpnCalculator/RpnTypes.fs
@@ -0,0 +1,14 @@
+﻿module internal RpnTypes
+
+type Number =
+    | Integer of int
+    | Float of float
+
+type RpnItem = 
+    | Operand of Number
+    | Operator of string
+
+type Operation =
+    | BinaryOperation of (string * Number -> Number -> Number)
+
+
