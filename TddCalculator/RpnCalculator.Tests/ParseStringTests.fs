1e5ce406a3d54fde7962eaa6f2faa09533469371
zpbappi <zpbappi@gmail.com>
2015-08-02 22:52:54 +1000

    implementation of parse

---
+++ TddCalculator/RpnCalculator.Tests/ParseStringTests.fs
@@ -0,0 +1,36 @@
+﻿module RpnCalculatorTests.ParseString
+
+open Xunit
+open Swensen.Unquote
+open RpnTypes
+open RpnCalculator
+
+[<Fact>]
+let ``should return nothing for empty or null strings`` () =
+    test <@ Utility.parse null = None @>
+    test <@ Utility.parse "" = None @>
+
+[<Theory>]
+[<InlineData("0", 0)>]
+[<InlineData("1", 1)>]
+[<InlineData("42", 42)>]
+[<InlineData("-1", -1)>]
+[<InlineData("-390", -390)>]
+let ``should parse interger numbers correctly`` (str : string) (num : int) =
+    test <@ Utility.parse str = Some (Operand (Integer num)) @>
+
+[<Theory>]
+[<InlineData("1.2", 1.2f)>]
+[<InlineData("3.1415", 3.1415f)>]
+[<InlineData("-4.2", -4.2f)>]
+[<InlineData("-1.", -1.f)>]
+let ``should parse float numbers correctly`` (str : string) (num : float) =
+    test <@ Utility.parse str = Some (Operand (Float num)) @>
+
+[<Theory>]
+[<InlineData("+")>]
+[<InlineData("-")>]
+[<InlineData("++")>]
+[<InlineData("may-be-operator")>]
+let ``should parse operators correctly`` (op : string) =
+    test <@ Utility.parse op = Some (Operator op) @>
--- TddCalculator/RpnCalculator/RpnCalculator.fs
+++ TddCalculator/RpnCalculator/RpnCalculator.fs
@@ -1,8 +1,31 @@
 ﻿module RpnCalculator
 
 open Stack
+open RpnTypes
+
+module Utility =
+    open RpnTypes
+
+    let private (|IsInt|_|) (str : string) =
+        match System.Int32.TryParse(str) with
+        | (success, number) when success -> Some number
+        | _ -> None
+
+    let private (|IsFloat|_|) (str : string) =
+        match System.Single.TryParse(str) with
+        | (success, number) when success -> Some (float number)
+        | _ -> None
+
+    let parse (str : string) : RpnItem option =
+        match str with 
+        | null | "" -> None
+        | IsInt x -> Some (Operand (Integer x))
+        | IsFloat f -> Some (Operand (Float f))
+        | _ -> Some (Operator str)
+
 
 let calculate (stack : Stack<string>) =
+    let x = Utility.parse "2"
     match stack with
     | [a; b; "+"] -> System.Int32.Parse(a) + System.Int32.Parse(b)
     | [a; b; "-"] -> System.Int32.Parse(a) - System.Int32.Parse(b)
